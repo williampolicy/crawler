57ea2f30f78c0372684104138c41a2c7b7404444
Willaimcode <kangxiaowen@gmail.com>
2023-04-04 15:51:51 -0400

    change app. try to use in pythonanywhere

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 import pymysql
 import csv
 from prettytable import PrettyTable
-# import matplotlib.pyplot as plt
-# from io import BytesIO
-# import base64
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 
@@ -19,10 +20,12 @@ def index():
 
     # 从 CSV 文件中读取数据并创建表格
     table = create_table()
+    img = create_bar_chart()
+    img_base64 = base64.b64encode(img.getvalue()).decode('ascii')  # 需要做格式转换
 
-    return render_template('index.html', table=table.get_html_string())
-    # img_data=img_base64 改动后无需 img_data 变量
 
+
+    return render_template('index.html', table=table.get_html_string(), img_data=img_base64)
 
 def is_int(value):
     try:
@@ -32,25 +35,59 @@ def is_int(value):
         return False
 
 def create_table():
+    data = []
+    with open('output_wiki.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.append(row)
+
+    data.pop(0)  # 删除表头行
+
+    filtered_data = [row for row in data[:10] if is_int(row[4].replace(',', ''))]
+
+    # 创建 PrettyTable 实例
+    table = PrettyTable()
+    table.field_names = ["Country", "Population (2023)"]
+    for row in filtered_data:
+        table.add_row([row[0], row[4]])
+
+    return table
+
+def create_bar_chart():
     with open('output_wiki.csv', 'r') as f:
         reader = csv.reader(f)
         data = [row for row in reader]
 
     data.pop(0)  # 删除表头行
+    # 测试展示
+    # for row in data:
+    #     category = row[0]
+    #     value = row[4]
+    #     print(f"{category}: {value}")
 
-    filtered_data = [row for row in data[:10] if is_int(row[5].replace(',', ''))]
-    
-    # 创建 PrettyTable 实例
-    table = PrettyTable()
-    table.field_names = ["Country", "Population (2023)"]
-    for row in filtered_data:
-        table.add_row([row[1], row[5]])
+    def is_int(value):
+        try:
+            int(value)
+            return True
+        except ValueError:
+            return False
 
-    return table
+    filtered_data = [row for row in data if is_int(row[4].replace(',', ''))]
+    sorted_data = sorted(filtered_data, key=lambda x: int(x[4].replace(',', '')), reverse=True)[:10]
 
-# 注释掉创建图表的相关函数
-# def create_bar_chart():
-# ...
+    countries = [row[0] for row in sorted_data]
+    population_2023 = [int(row[4].replace(',', '')) for row in sorted_data]
+
+    fig = plt.figure()
+    plt.bar(countries, population_2023)
+    plt.xlabel("Countries")
+    plt.ylabel("Population (2023)")
+    plt.xticks(rotation=45)
+
+    img = BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    return img
 
 
 if __name__ == '__main__':
